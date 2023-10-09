206842e6f9fca9143f95e7af6694c9f33c369000
Maxuel Vieira Tobá Junior <maxueljunior@weg.net>
2023-10-09 13:16:52 -0300

    Inicializando projeto

---
+++ src/test/java/br/com/leuxam/acougue/AcougueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leuxam.acougue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcougueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
