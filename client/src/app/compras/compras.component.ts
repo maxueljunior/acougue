356b94781f69d5855ccb80796b70075ce7f96746
Maxuel Vieira Tobá Junior <maxueltstz@hotmail.com>
2024-01-23 19:21:39 -0300

    Adicionando input de file e realizando algumas alterações

--- client/src/app/compras/compras.component.ts
+++ client/src/app/compras/compras.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CompraEstoque } from '../core/types/ComprasEstoque';
 import { Responsivo } from '../core/types/Types';
@@ -25,7 +25,10 @@ export class ComprasComponent {
   displayedColumns: string[] = ['id', 'idEstoque', 'quantidade', 'precoUnitario'];
   displayedesColumns: string[] = ['id', 'idEstoque', 'quantidade', 'precoUnitario'];
 
-  criarCompra: boolean = false;
+  selectedFileName: string = '';
+  @ViewChild('fileInput') fileInput: ElementRef | undefined;
+
+  criarCompra: boolean = true;
 
   colunas: Responsivo[] = [
     {nome: "Nº", atributo: "id"},
@@ -41,8 +44,34 @@ export class ComprasComponent {
     {nome: "Valor Unit.", atributo: "precoUnitario"},
   ]
 
+  constructor(
+    private renderer: Renderer2
+  ){
+
+  }
+
   criarCompras(): void{
     this.criarCompra = true;
+  }
+
+  selectFile(): void {
+    // Simula o clique no input de arquivo oculto
+    if (this.fileInput) {
+      this.renderer.selectRootElement(this.fileInput.nativeElement).click();
+    }
+  }
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      console.log('Arquivo selecionado:', file);
+
+      // Atualiza o nome do arquivo exibido no campo
+      this.selectedFileName = file.name;
+
+      // Faça o que quiser com o arquivo aqui, como enviá-lo para o servidor, etc.
+    }
   }
 }
 
